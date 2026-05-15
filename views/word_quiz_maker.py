d0c1879cf3acaea0e896678c83ef58af833072de
KEI20030212 <tkey4392@gmail.com>
2026-05-15 16:53:22 +0900

    Add files via upload

--- views/word_quiz_maker.py
+++ views/word_quiz_maker.py
@@ -59,7 +59,7 @@ def render_word_quiz_maker_page():
         elif quiz_name == "キクタン16問":
             q_range, a_range, p_size = "A1:I18", "J1:R18", "B5"
         elif quiz_name == "キクタン32問":
-            q_range, a_range, p_size = "A1:L18", "M1:Y18", "A4"
+            q_range, a_range, p_size = "A1:K18", "L1:X18", "A4"
         else: # キクタン50問
             q_range, a_range, p_size = "A1:N27", "O1:AB27", "A3"
 
